e9bd9fef4c2f7c173e09fc670aca435944e15b7d
Nv8x0d4L <Nv8x0d4L@users.noreply.github.com>
2016-04-12 12:51:59 +0200

    hello world

---
+++ meteosat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meteosat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello, world!");
+        }
+    }
+}
